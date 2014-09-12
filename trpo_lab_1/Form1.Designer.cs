0719aaf888a949724f2e57a3c321c0c9f320f444
alexeyk23 <kunak94@mail.ru>
2014-09-12 12:00:05 +0300

    TRPO lab_1 Lexical analizator

---
+++ trpo_lab_1/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace trpo_lab_1
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txbxExpression = new System.Windows.Forms.TextBox();
+            this.btnGoAutomat = new System.Windows.Forms.Button();
+            this.txbxAnswer = new System.Windows.Forms.TextBox();
+            this.lblExpr = new System.Windows.Forms.Label();
+            this.btnClearExpr = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txbxExpression
+            // 
+            this.txbxExpression.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txbxExpression.Location = new System.Drawing.Point(12, 34);
+            this.txbxExpression.Name = "txbxExpression";
+            this.txbxExpression.Size = new System.Drawing.Size(347, 29);
+            this.txbxExpression.TabIndex = 0;
+            // 
+            // btnGoAutomat
+            // 
+            this.btnGoAutomat.Location = new System.Drawing.Point(89, 69);
+            this.btnGoAutomat.Name = "btnGoAutomat";
+            this.btnGoAutomat.Size = new System.Drawing.Size(132, 29);
+            this.btnGoAutomat.TabIndex = 1;
+            this.btnGoAutomat.Text = "Запустить анализатор";
+            this.btnGoAutomat.UseVisualStyleBackColor = true;
+            this.btnGoAutomat.Click += new System.EventHandler(this.btnGoAutomat_Click);
+            // 
+            // txbxAnswer
+            // 
+            this.txbxAnswer.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txbxAnswer.Location = new System.Drawing.Point(12, 104);
+            this.txbxAnswer.Multiline = true;
+            this.txbxAnswer.Name = "txbxAnswer";
+            this.txbxAnswer.ReadOnly = true;
+            this.txbxAnswer.Size = new System.Drawing.Size(347, 232);
+            this.txbxAnswer.TabIndex = 2;
+            // 
+            // lblExpr
+            // 
+            this.lblExpr.AutoSize = true;
+            this.lblExpr.Location = new System.Drawing.Point(13, 15);
+            this.lblExpr.Name = "lblExpr";
+            this.lblExpr.Size = new System.Drawing.Size(110, 13);
+            this.lblExpr.TabIndex = 3;
+            this.lblExpr.Text = "Введите выражение";
+            // 
+            // btnClearExpr
+            // 
+            this.btnClearExpr.Location = new System.Drawing.Point(227, 69);
+            this.btnClearExpr.Name = "btnClearExpr";
+            this.btnClearExpr.Size = new System.Drawing.Size(132, 29);
+            this.btnClearExpr.TabIndex = 4;
+            this.btnClearExpr.Text = "Очистить выражение";
+            this.btnClearExpr.UseVisualStyleBackColor = true;
+            this.btnClearExpr.Click += new System.EventHandler(this.btnClearExpr_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(375, 352);
+            this.Controls.Add(this.btnClearExpr);
+            this.Controls.Add(this.lblExpr);
+            this.Controls.Add(this.txbxAnswer);
+            this.Controls.Add(this.btnGoAutomat);
+            this.Controls.Add(this.txbxExpression);
+            this.Name = "frmMain";
+            this.Text = "Чудо-автомат";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txbxExpression;
+        private System.Windows.Forms.Button btnGoAutomat;
+        private System.Windows.Forms.TextBox txbxAnswer;
+        private System.Windows.Forms.Label lblExpr;
+        private System.Windows.Forms.Button btnClearExpr;
+    }
+}
+
---
+++ trpo_lab_1/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace trpo_lab_1
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+        HashSet<char> setOfSigns = new HashSet<char>() { '+', '-', '/', '*' };
+        HashSet<char> setOfDif = new HashSet<char>() { '<', '>', '=' };
+        HashSet<char> setOfBrakes = new HashSet<char>() { '(', ')', '[', ']','{','}'};
+        HashSet<string> setOfReservedWords = new HashSet<string> { "if", "else", "then", "and", "or", "while", "repeat", "do", "until" };
+        List<string> answer = new List<string>();
+        private void btnGoAutomat_Click(object sender, EventArgs e)
+        {
+            runAutomat(txbxExpression.Text);
+            txbxAnswer.Text = "";            
+            foreach (string item in answer)
+            {               
+                txbxAnswer.Text += item + Environment.NewLine;
+            }
+            answer.Clear();
+        }
+        void runAutomat(string s)
+        {
+            int i = 0;
+            StringBuilder tmp = new StringBuilder(s[i]);
+            while (i < s.Length)
+            {
+                tmp.Append(s[i]);
+                if (s[i] == ' ')
+                    i++;
+                else
+                    if (Char.IsLetter(s[i]))//если буква
+                    {
+                        i++;
+                        //пока буква или цифра
+                        while (i < s.Length && (Char.IsLetter(s[i]) || Char.IsDigit(s[i])))
+                        {
+                            tmp.Append(s[i]);
+                            i++;
+                        }
+                        if (setOfReservedWords.Contains(tmp.ToString()))
+                        {
+                            answer.Add(tmp.ToString() + "\t служебное слово");
+                        }
+                        else //это не служебное слово, проверим, идет ли после оператор присваивания
+                        {
+                            answer.Add(tmp.ToString() + "\t идентификатор");
+                            while (i < s.Length && s[i] == ' ')
+                                i++;
+                            if (i < s.Length - 2 && s[i] == ':' && s[i + 1] == '=')
+                            {
+                                answer.Add(s[i].ToString() + s[i + 1].ToString() + "\t оператор присваивания");
+                                i += 2;
+                            }
+
+                        }
+
+                    }
+                    else
+                        if (Char.IsDigit(s[i]))//если цифра
+                        {
+                            i++;
+                            while (i < s.Length && Char.IsDigit(s[i]))//пока цифры
+                            {
+                                tmp.Append(s[i]);
+                                i++;
+                            }
+                            if (i < s.Length - 2 && s[i] == '.' && Char.IsDigit(s[i + 1]))//если нашли точку и после нее цифра
+                            {
+                                tmp.Append(s[i]);
+                                i++;
+                                while (i < s.Length && Char.IsDigit(s[i]))
+                                {
+                                    tmp.Append(s[i]);
+                                    i++;
+                                }
+                                answer.Add(tmp.ToString() + "\t вещественное число");
+                            }
+                            else
+                                answer.Add(tmp.ToString() + "\t целое число");
+                        }
+                        else
+                            if (setOfSigns.Contains(s[i]))
+                            {
+                                answer.Add(s[i] + "\t арифметическая операция");
+                                i++;
+                            }
+                            else
+                                if (setOfDif.Contains(s[i]))
+                                {
+                                    answer.Add(s[i] + "\t операция сравнения");
+                                    i++;
+                                }
+                                else
+                                    if (s[i] == '.')
+                                    {
+                                        answer.Add(s[i] + "\t точка");
+                                        i++;
+                                    }
+                                    else
+                                        if (setOfBrakes.Contains(s[i]))
+                                        {
+                                            answer.Add(s[i] + "\t скобка");
+                                            i++;
+                                        }
+                                        else
+                                        {
+                                            answer.Add(s[i] + "\t неизвестный символ");
+                                            i++;
+                                        }
+                tmp.Clear();
+            }
+
+        }
+
+        private void btnClearExpr_Click(object sender, EventArgs e)
+        {
+            txbxExpression.Text = "";
+            txbxAnswer.Text="";
+
+        }
+    }
+}
